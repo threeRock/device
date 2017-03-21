128cce3c8fad0601182d1c197b9a3f646d31ff6e
tongtn <versnoontt02@gmail.com>
2017-03-21 10:39:08 +0800

    初始化项目

---
+++ src/main/java/io/jianxun/DeviceApplication.java
@@ -0,0 +1,12 @@
+package io.jianxun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceApplication.class, args);
+	}
+}
