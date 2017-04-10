11032ad67e54e0a2fb2cc86436b7b78025be9e2f
tongtn <versnoontt02@gmail.com>
2017-04-10 10:23:39 +0800

    增加用户查询描述

--- src/main/java/io/jianxun/service/user/UserPredicates.java
+++ src/main/java/io/jianxun/service/user/UserPredicates.java
@@ -4,8 +4,14 @@ import com.querydsl.core.types.Predicate;
 
 import io.jianxun.domain.business.user.QUser;
 
+/**
+ * 用户相关查询条件
+ * @author Administrator
+ *
+ */
 public class UserPredicates {
 
+	//用户名查询
 	public static Predicate usernamePredicate(String username) {
 		QUser user = QUser.user;
 		return user.username.eq(username);
